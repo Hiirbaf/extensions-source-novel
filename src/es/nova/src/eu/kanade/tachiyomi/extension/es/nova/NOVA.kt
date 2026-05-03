e81b973423c74fef5d3b631bcdbe97474c588356
Hiirbaf <42479509+Hiirbaf@users.noreply.github.com>
2026-05-02 22:29:56 -0300

    Add imports for network and utility functions

--- src/es/nova/src/eu/kanade/tachiyomi/extension/es/nova/NOVA.kt
+++ src/es/nova/src/eu/kanade/tachiyomi/extension/es/nova/NOVA.kt
@@ -1,10 +1,13 @@
 package eu.kanade.tachiyomi.extension.es.nova
 
+import eu.kanade.tachiyomi.network.GET
+import eu.kanade.tachiyomi.network.asObservableSuccess
 import eu.kanade.tachiyomi.source.model.FilterList
 import eu.kanade.tachiyomi.source.model.Page
 import eu.kanade.tachiyomi.source.model.SChapter
 import eu.kanade.tachiyomi.source.model.SManga
 import eu.kanade.tachiyomi.source.online.ParsedHttpSource
+import eu.kanade.tachiyomi.util.asJsoup
 import okhttp3.Request
 import okhttp3.Response
 import org.jsoup.Jsoup
