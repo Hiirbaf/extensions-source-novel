2f8ce5e42db97eac4743955df1ae00514c6c1cc6
Hiirbaf <42479509+Hiirbaf@users.noreply.github.com>
2026-05-02 22:57:51 -0300

    Add import for NovelSource in NOVA.kt

--- src/es/nova/src/eu/kanade/tachiyomi/extension/es/nova/NOVA.kt
+++ src/es/nova/src/eu/kanade/tachiyomi/extension/es/nova/NOVA.kt
@@ -2,6 +2,7 @@ package eu.kanade.tachiyomi.extension.es.nova
 
 import eu.kanade.tachiyomi.network.GET
 import eu.kanade.tachiyomi.network.await
+import eu.kanade.tachiyomi.source.NovelSource
 import eu.kanade.tachiyomi.source.model.FilterList
 import eu.kanade.tachiyomi.source.model.Page
 import eu.kanade.tachiyomi.source.model.SChapter
